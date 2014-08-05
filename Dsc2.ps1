baee4a21a4b2658f24fd9b3a3f53973377e3b385
David Gardiner <david@gardiner.net.au>
2014-08-05 13:58:22 +0930

    DSC Demo scripts

---
+++ Dsc2.ps1
@@ -0,0 +1,47 @@
+﻿Set-Location c:\dev\PowerShellTalk
+
+$dtc = $false
+
+Configuration Win8
+{
+    Node "169.254.58.142"
+    {
+        LocalConfigurationManager
+        {
+            # Credential = $cred
+            RebootNodeIfNeeded = $true
+            AllowModuleOverwrite = $false
+        }
+
+        File CustomDirectory
+        {
+            Ensure = "Present" 
+            Type = "Directory“ 
+            DestinationPath = "C:\tmp"
+        }
+
+        if ($dtc)
+        {
+            Service MSDTC
+            {
+                Name = "MSDTC"
+                StartupType = "Manual"
+                State = "Running"
+            }
+        }
+    }
+}
+
+Win8
+
+# Go to Win8a and run Get-DscLocalConfigurationManager
+# Show Windows Explorer 
+
+# Set-DscLocalConfigurationManager .\Win8
+
+# winrm get winrm/config/client
+# winrm set winrm/config/client '@{TrustedHosts="169.254.58.142"}'
+
+# Set-DscLocalConfigurationManager .\Win8 -Credential (Get-Credential)
+
+# Start-DscConfiguration .\Win8 -Wait -Verbose -Credential (Get-Credential)
---
+++ Dsc2b.ps1
@@ -0,0 +1,68 @@
+﻿Set-Location c:\dev\PowerShellTalk
+
+$configData = @{
+    AllNodes = @(
+        @{
+            NodeName = "*"
+            PSDscAllowPlainTextPassword = $true
+        }
+        @{
+            NodeName = "169.254.58.142";
+        }
+#        @{
+#            NodeName = "169.254.223.157";
+#        }
+    )
+}
+
+Configuration Win8b
+{
+    param ([PSCredential] $credential)
+
+    Node $AllNodes.NodeName
+    {
+        LocalConfigurationManager
+        {
+            Credential = $credential
+            RebootNodeIfNeeded = $true
+            AllowModuleOverwrite = $false
+        }
+
+        File CustomDirectory
+        {
+            Ensure = "Present" 
+            Type = "Directory“ 
+            DestinationPath = "C:\tmp"
+        }
+
+        Service MSDTC
+        {
+            Name = "MSDTC"
+            StartupType = "Manual"
+            State = "Running"
+        }
+
+        File CopyFile
+        {
+            Type = "File"
+            SourcePath = "\\169.254.5.36\c$\Downloads\7z920-x64.msi"
+            DestinationPath = "c:\tmp\7z920-x64.msi"
+            Credential = $credential
+            DependsOn = "[File]CustomDirectory"
+        }
+
+        Package 7Zip
+        {
+            Name = "7Zip"
+            Path = "c:\tmp\7z920-x64.msi"
+            ProductID = "{23170F69-40C1-2702-0920-000001000000}"
+            DependsOn = "[File]CopyFile"
+        }
+    }
+}
+
+Win8b -ConfigurationData $ConfigData -credential (Get-credential)
+
+# winrm set winrm/config/client '@{TrustedHosts="169.254.58.142,169.254.223.157"}'
+
+# Start-DscConfiguration .\Win8 -Wait -Verbose -Credential (Get-Credential)
